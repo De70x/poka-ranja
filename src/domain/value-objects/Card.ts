317a3283476d7dc1167155a75844e6f7cdb59630
deloffre <antoine.deloffre@kelio.com>
2025-08-25 14:26:06 +0200

    Cards with compare

--- src/domain/value-objects/Card.ts
+++ src/domain/value-objects/Card.ts
@@ -1,22 +1,65 @@
 export enum Suit {
-    SPADES = 's',
-    HEARTS = 'h',
-    DIAMONDS = 'd',
-    CLUBS = 'c'
+  SPADES = 's',
+  HEARTS = 'h',
+  DIAMONDS = 'd',
+  CLUBS = 'c'
 }
 
 export enum Rank {
-    TWO = '2', THREE = '3', FOUR = '4', FIVE = '5', SIX = '6',
-    SEVEN = '7', EIGHT = '8', NINE = '9', TEN = 'T',
-    JACK = 'J', QUEEN = 'Q', KING = 'K', ACE = 'A'
+  TWO = '2', THREE = '3', FOUR = '4', FIVE = '5', SIX = '6',
+  SEVEN = '7', EIGHT = '8', NINE = '9', TEN = 'T',
+  JACK = 'J', QUEEN = 'Q', KING = 'K', ACE = 'A'
 }
 
+
+
+
 export class Card {
-    constructor(private readonly rank: Rank, private readonly suit: Suit) {
+  private static readonly RANK_VALUES: Record<Rank, number> = {
+    [Rank.TWO]: 2,
+    [Rank.THREE]: 3,
+    [Rank.FOUR]: 4,
+    [Rank.FIVE]: 5,
+    [Rank.SIX]: 6,
+    [Rank.SEVEN]: 7,
+    [Rank.EIGHT]: 8,
+    [Rank.NINE]: 9,
+    [Rank.TEN]: 10,
+    [Rank.JACK]: 11,
+    [Rank.QUEEN]: 12,
+    [Rank.KING]: 13,
+    [Rank.ACE]: 14
+  };
 
+  constructor(private readonly rank: Rank, private readonly suit: Suit) {
+  }
+
+  getRank(): Rank {
+    return this.rank
+  }
+
+  getRankValue(): number {
+    return Card.RANK_VALUES[this.getRank()]
+  }
+
+  getSuit(): Suit {
+    return this.suit
+  }
+
+  compareTo(card: Card): number {
+    if(this.getRankValue() > card.getRankValue()){
+      return -1
     }
+    else if(this.getRankValue() < card.getRankValue()){
+      return 1
+    }
+    else{
+      return 0
+    }
+  }
 
-    getRank(): Rank { return this.rank }
-    getSuit(): Suit { return this.suit }
+  static sortCards(card1:Card, card2:Card){
+    return card1.compareTo(card2)
+  }
 
 }
--- src/tests/domain/value-objects/Card.test.ts
+++ src/tests/domain/value-objects/Card.test.ts
@@ -1,12 +1,40 @@
 import {describe, it, expect} from "vitest";
 import {Card, Rank, Suit} from "../../../domain/value-objects/Card.js";
 
+const areArraysEquals = (array1, array2) => {
+  return array1.length === array2.length && array1.every((value, index) => value === array2[index])
+}
+
 describe("Card", () => {
-    describe("constructor", () => {
-        it("should create a Card if rank and suits are correct", () => {
-            const card = new Card(Rank.SEVEN, Suit.SPADES)
-            expect(card.getRank()).toBe(Rank.SEVEN)
-            expect(card.getSuit()).toBe(Suit.SPADES)
-        })
+  describe("constructor", () => {
+    it("should create a Card if rank and suits are correct", () => {
+      const card = new Card(Rank.SEVEN, Suit.SPADES)
+      expect(card.getRank()).toEqual(Rank.SEVEN)
+      expect(card.getSuit()).toEqual(Suit.SPADES)
     })
+  })
+  describe("compare", () => {
+    it("should be able to compare a rank", () => {
+      const card1 = new Card(Rank.SEVEN, Suit.SPADES)
+      const card2 = new Card(Rank.TWO, Suit.SPADES)
+      const card3 = new Card(Rank.TWO, Suit.CLUBS)
+      expect(card1.compareTo(card2)).toEqual(-1)
+      expect(card2.compareTo(card1)).toEqual(1)
+      expect(card3.compareTo(card2)).toEqual(0)
+    })
+    it("should be able to sort card arrays", () => {
+      const cards = []
+      cards.push(new Card(Rank.SEVEN, Suit.SPADES))
+      cards.push(new Card(Rank.SEVEN, Suit.CLUBS))
+      cards.push(new Card(Rank.ACE, Suit.CLUBS))
+      cards.push(new Card(Rank.TWO, Suit.CLUBS))
+      cards.push(new Card(Rank.TEN, Suit.CLUBS))
+
+      cards.sort((a, b) => Card.sortCards(a,b))
+
+      const expectedOrder = ['A', 'T', '7', '7', '2']
+      const realOrder = cards.map(c => c.rank)
+      expect(areArraysEquals(expectedOrder, realOrder)).toEqual(true)
+    })
+  })
 })
