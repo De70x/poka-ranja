d3c66c0ec42c540c8634c1da11630b1210c117dc
De70x <antoine.deloffre7@gmail.com>
2025-08-26 05:35:44 +0200

    Hand domain tests passing

--- src/domain/value-objects/Hand.ts
+++ src/domain/value-objects/Hand.ts
@@ -1,7 +1,25 @@
 import {Card} from "./Card.js";
 
+export enum HAND_TYPES {
+    PAIRED = '',
+    OFFSUITED = 'o',
+    SUITED = 's',
+}
+
 export class Hand {
   constructor(private readonly card1: Card, private readonly card2: Card) {
+  }
+
+  getType(): HAND_TYPES {
+      if(this.card1.getRank() === this.card2.getRank()) {
+          return HAND_TYPES.PAIRED
+      }
+      else if(this.card1.getSuit() === this.card2.getSuit()) {
+          return HAND_TYPES.SUITED
+      }
+      else{
+          return HAND_TYPES.OFFSUITED
+      }
   }
 
 }
