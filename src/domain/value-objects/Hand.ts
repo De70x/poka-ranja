021b8eb802bb129705c051c1070ba4ca08ee9637
De70x <antoine.deloffre7@gmail.com>
2025-08-27 06:44:18 +0200

    Hand WIP since we change everything

--- src/domain/value-objects/Hand.ts
+++ src/domain/value-objects/Hand.ts
@@ -1,4 +1,4 @@
-import {Card} from "./Card.js";
+import {Rank} from "./Card.js";
 
 export enum HAND_TYPES {
   PAIRED = '',
@@ -7,16 +7,24 @@ export enum HAND_TYPES {
 }
 
 export class Hand {
-  constructor(private readonly card1: Card, private readonly card2: Card) {
+  constructor(private readonly name: string) {
+    if (!Object.values(Rank).includes(name.charAt(0) as Rank) || !Object.values(Rank).includes(name.charAt(1) as Rank)) {
+      throw new Error('DOMAIN_EXCEPTION_UNKNOWN_HAND');
+    }
+    if (name.charAt(0) === name.charAt(1) && name.length !== 2) {
+      throw new Error('DOMAIN_EXCEPTION_UNKNOWN_HAND');
+    }
   }
   
   public getType(): HAND_TYPES {
-    if (this.card1.getRank() === this.card2.getRank()) {
+    if (this.name.length === 2 && this.name.charAt(0) === this.name.charAt(1)) {
       return HAND_TYPES.PAIRED
-    } else if (this.card1.getSuit() === this.card2.getSuit()) {
+    } else if (this.name.charAt(2) === 's') {
       return HAND_TYPES.SUITED
-    } else {
+    } else if (this.name.charAt(2) === 'o') {
       return HAND_TYPES.OFFSUITED
+    } else {
+      throw new Error(`Unhandled hand ${this.name}`)
     }
   }
 }
--- src/tests/domain/value-objects/Hand.test.ts
+++ src/tests/domain/value-objects/Hand.test.ts
@@ -1,26 +1,26 @@
 import {describe, expect, it} from "vitest";
-import {Card, Rank, Suit} from "../../../domain/value-objects/Card.js";
 import {Hand, HAND_TYPES} from "../../../domain/value-objects/Hand.js";
 
 describe("Hand", () => {
   describe("constructor", () => {
-    it('should say when it is a pair if two casds got the same rank', () => {
-      const card1 = new Card(Rank.SEVEN, Suit.CLUBS)
-      const card2 = new Card(Rank.SEVEN, Suit.SPADES)
-      const hand = new Hand(card1, card2)
+    it('should say when it is a pair if two cards got the same rank', () => {
+      const hand = new Hand('AA')
       expect(hand.getType()).toEqual(HAND_TYPES.PAIRED)
     })
-    it('should say when it is suited if two cards got the same suit', () => {
-      const card1 = new Card(Rank.SEVEN, Suit.SPADES)
-      const card2 = new Card(Rank.ACE, Suit.SPADES)
-      const hand = new Hand(card1, card2)
+    it('should say when it is suited if last char is s', () => {
+      const hand = new Hand('AKs')
       expect(hand.getType()).toEqual(HAND_TYPES.SUITED)
     })
-    it('should say when it is offsuited if two cards does not have the same rank nor suit', () => {
-      const card1 = new Card(Rank.SEVEN, Suit.SPADES)
-      const card2 = new Card(Rank.ACE, Suit.CLUBS)
-      const hand = new Hand(card1, card2)
+    it('should say when it is offsuited last char is o', () => {
+      const hand = new Hand('AKo')
       expect(hand.getType()).toEqual(HAND_TYPES.OFFSUITED)
+    })
+    it('should not accept other char than known Ranks and o and s', () => {
+      expect(() => new Hand('AP')).toThrow('DOMAIN_EXCEPTION_UNKNOWN_HAND')
+    })
+    it('should not accept to have the same card with an additional char (AAs is dumb and AAo is too long)', () => {
+      expect(() => new Hand('AAs')).toThrow('DOMAIN_EXCEPTION_UNKNOWN_HAND')
+      expect(() => new Hand('AAo')).toThrow('DOMAIN_EXCEPTION_UNKNOWN_HAND')
     })
   })
 })
