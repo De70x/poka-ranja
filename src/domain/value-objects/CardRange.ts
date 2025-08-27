84b865395f4866e97b997c51dbfa2e1e9fa347a5
De70x <antoine.deloffre7@gmail.com>
2025-08-27 06:18:31 +0200

    one commit per it :
    correct :
    expect(range.getHands().length).toEqual(169)
    AssertionError: expected +0 to deeply equal 169
    
    dumb loop of 169 it with the same card

--- src/domain/value-objects/CardRange.ts
+++ src/domain/value-objects/CardRange.ts
@@ -1,9 +1,13 @@
 import {Hand} from "./Hand.js";
+import {Card, Rank, Suit} from "./Card.js";
 
 export class CardRange {
   private readonly hands: Hand[] = [];
   
   constructor(private readonly name: string) {
+    for (let i = 0; i < 169; i++) {
+      this.hands.push(new Hand(new Card(Rank.ACE, Suit.CLUBS), new Card(Rank.ACE, Suit.CLUBS)))
+    }
   }
   
   public getHands() {
